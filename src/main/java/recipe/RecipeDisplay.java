f2565e625df9e24d8960507709a72e2f547b858d
H.Y <93350299+yueY2003@users.noreply.github.com>
2022-11-15 22:29:07 -0500

    Version1

---
+++ src/main/java/recipe/RecipeDisplay.java
@@ -0,0 +1,19 @@
+package recipe;
+//This class is the presenter that visualizes the Recipe. It creates a simple UI that allow users to enter the information
+//desired to create a new recipe.
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class RecipeDisplay {
+    // This class
+    String name;
+    String picture;
+    ArrayList<Map<String, Integer>> ingredients;
+    String steps;
+
+    JFrame f = new JFrame();
+    JButton b = new JButton("click");
+
+}
