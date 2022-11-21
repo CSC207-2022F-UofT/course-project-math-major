c80746f8d0efb59f1c73434dc663c73fe4a8e68f
ChangMa-Chauncey <chang.ma@mail.utoronto.ca>
2022-11-21 18:51:53 -0500

    Finished controller, interactor.

--- src/main/java/controller/RecipeRatingController.java
+++ src/main/java/controller/RecipeRatingController.java
@@ -1,5 +1,15 @@
-package controller;
+package Controller;
+
+import RatedUseCase.RatedInteractor;
+import gateway.AccountGateway;
+import gateway.AccountGatewayImplementation;
+import gateway.RatedGateway;
+
+import java.io.IOException;
 
 public abstract class RecipeRatingController {
-
+    public void RecipeRatingController(String UserID) throws IOException {
+        RatedInteractor ratinginteractor = new RatedInteractor();
+        ratinginteractor.RatedInteractor(UserID);
+    }
 }
