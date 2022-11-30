a8be3119862a8290f8a36938b269e4b714ac1edc
lindasun <lindayx.sun@mail.utoronto.ca>
2022-11-30 16:11:57 -0500

    add searching function

--- src/main/java/ranking_use_case/RankingInteractor.java
+++ src/main/java/ranking_use_case/RankingInteractor.java
@@ -35,3 +35,6 @@ public class RankingInteractor implements RankingInputBoundary {
         return rankingResponseModel;
     }
 }
+
+
+
---
+++ src/main/java/ui/SearchDisplay.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+import entity.Rank;
+import Searching_use_case.SearchingResponseModel;
+
+public class SearchDisplay extends JFrame{
+    public SearchDisplay(){
+        this.setTitle("search");
+        this.setLayout(null);
+        this.setSize(1000,800);
+
+    }
+}
